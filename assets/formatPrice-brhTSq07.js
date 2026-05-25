e892ae4c13e13ae3f85d96bbc8beec4742d18d63
shaheer <www.shaheerkhalid88600@gmail.com>
2026-05-25 09:54:57 +0500

    deploy

---
+++ assets/formatPrice-brhTSq07.js
@@ -0,0 +1,2 @@
+function m(r,t="USD",i="en-US"){return new Intl.NumberFormat(i,{style:"currency",currency:t,minimumFractionDigits:2,maximumFractionDigits:2}).format(r)}export{m as f};
+//# sourceMappingURL=formatPrice-brhTSq07.js.map
